c3bf6571190f8dc77fbbd651df47ddc33794a0c7
Louis Hawkins <louishawkins@gmail.com>
2014-12-16 15:50:38 -0600

    removed ondelete

--- app/database/migrations/2014_12_17_213816_create_projects_table.php
+++ app/database/migrations/2014_12_17_213816_create_projects_table.php
@@ -23,13 +23,13 @@ class CreateProjectsTable extends Migration {
 			$table->decimal('funds_current', 11, 2);
 			$table->decimal('funds_goal', 11, 2);
 			$table->integer('genre_id')->unsigned();
-			$table->foreign('genre_id')->references('id')->on('genres')->onDelete('cascade');
-			$table->integer('owner_id')->unsigned();
-			$table->foreign('owner_id')->references('id')->on('users')->onDelete('cascade');
+			$table->foreign('genre_id')->references('id')->on('genres');
+			$table->integer('user_id')->unsigned();
+			$table->foreign('user_id')->references('id')->on('users');
 			$table->enum('stage', array('pre-production', 'in-production', 'post-production'));
 			$table->string('video_url');
 			$table->integer('content_id')->unsigned();
-			$table->foreign('content_id')->references('id')->on('contents')->onDelete('cascade')->nullable();
+			$table->foreign('content_id')->references('id')->on('contents')->nullable();
 			$table->timestamps();
 		});
 	}
