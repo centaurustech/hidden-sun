8f3dcbd184f53398cb243a27be1fc268f1ed613c
andrew zawora <drewzawora@yahoo.com>
2014-12-18 14:21:19 -0600

    seeder files

--- app/database/seeds/UsersTableSeeder.php
+++ app/database/seeds/UsersTableSeeder.php
@@ -11,7 +11,18 @@ class UsersTableSeeder extends Seeder {
 
 		foreach(range(1, 10) as $index)
 		{
-			User::create([
+			User::create([ 
+
+				'email'=>$faker->safeEmail,
+				'password'=>$faker->password,
+				'first_name'=>$faker->firstName,
+				'last_name'=>$faker->lastName,
+				'phone_number'=>$faker->phoneNumber,
+				'city'=>$faker->city,
+				'state'=>$faker->state,
+				'activation_code'=> str_random(60)
+				
+
 
 			]);
 		}
