cb0783c810bc934ced4f2b16285a2e3eef0d2357
andrew zawora <drewzawora@yahoo.com>
2014-12-18 14:24:30 -0600

    made project model

--- app/models/Project.php
+++ app/models/Project.php
@@ -4,10 +4,35 @@ class Project extends \Eloquent {
 
 	// Add your validation rules here
 	public static $rules = [
+		'project_title'		=> 'required|max:100',
+		'synopsis'			=> 'required|max:1000',
+		'start_date'		=> 'required',
+		'complete_date'		=> '',
+		'funds_end_date'	=> 'required',
+		'funds_current'		=> 'required',
+		'funds_goal'		=> 'required',
+		'stage'				=> 'required',
+		'video_url'			=> '',
+
+		];
 		// 'title' => 'required'
-	];
+
+	protected $table = 'projects';
+
+	public function projects()
+	{
+		return $this->hasMany('Project');
+	}
+
+	public function user()
+	{
+    	return $this->belongsTo('User');
+	}
+
 
 	// Don't forget to fill this array
 	protected $fillable = [];
 
 }
+
+  
