e57f4196a34ed0249ea98c13ff13f5da4ed8234d
andrew zawora <drewzawora@yahoo.com>
2014-12-22 09:28:46 -0600

    Drew , Update

--- app/views/projects/show.blade.php
+++ app/views/projects/show.blade.php
@@ -1,1 +1,2 @@
 /vagrant/sites/hidden-sun.dev/app/views/projects/show.blade.php
+
---
+++ app/views/projects/view.blade.php
@@ -0,0 +1,38 @@
+@extends('layouts.master')
+@section('content')
+<script type="text/javascript">
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.0/css/bootstrap.min.css">
+
+	<div class="row">
+	  <div class="col-md-1">.col-md-4</div>
+	  <div class="col-md-1">.col-md-4</div>
+	  <div class="col-md-1">.col-md-4</div>
+	  <div class="col-md-1">.col-md-4</div>
+	</div>
+	<div class="row">
+	  <div class="col-md-1">.col-md-4</div>
+	  <div class="col-md-1">.col-md-4</div>
+	  <div class="col-md-1">.col-md-4</div>
+	  <div class="col-md-1">.col-md-4</div>
+	</div>
+	<div class="row">
+	  <div class="col-md-1">.col-md-4</div>
+	  <div class="col-md-1">.col-md-4</div>
+	  <div class="col-md-1">.col-md-4</div>
+	  <div class="col-md-1">.col-md-4</div>
+	</div>
+	<div class="row">
+	  <div class="col-md-1">.col-md-4</div>
+	  <div class="col-md-1">.col-md-4</div>
+	  <div class="col-md-1">.col-md-4</div>
+	  <div class="col-md-1">.col-md-4</div>
+	</div>
+
+
+</script>
+
+
+@stop
