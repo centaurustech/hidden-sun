3db1e82f8bef14e3b72a33307a3b7a416d86711e
Josee LeChele Farmer <Jfarmer@Josees-MacBook-Pro.local>
2015-01-08 16:20:00 -0600

    fixed conflicts

--- app/views/projects/index.blade.php
+++ app/views/projects/index.blade.php
@@ -1,6 +1,5 @@
 @extends('layouts.master')
 @section('content')
-<<<<<<< HEAD
 <div class="col-md-2">
 <form id="browse-search-box" class="" method="get">
         <div id="browse-search">
@@ -11,8 +10,8 @@
         </div>
 </form>
 
-	<table>
-=======
+<table>
+
 <div class="container-fluid" style="margin-top:25px;margin-bottom:100px;">
 	<div class="row">
 		<div class="col-md-3">
@@ -80,69 +79,3 @@
 	</div>
 </div>
 @stop
-<!--
-<table class="table table-bordered" id="projects-table">
-	@foreach ($projects as $project)
->>>>>>> master
-		<tr>
-			<td>
-				<img src="{{ $project->thumbnail_url }}">
-			</td>
-			<td>
-				<table>
-					<tr><td><b>Title: </b>{{ $project->project_title }}</td></tr>
-					<tr><td><b>Genre: </b>
-						{{{ $project->genre_list }}}
-					</td></tr>
-					<tr><td><b>Goal: </b>{{ $project->funds_goal }}</td></tr>
-					<tr><td><b>Currently Funded: </b>{{ $project->funds_current }}</td></tr>
-					<tr><td><b>Funding Ends: </b>{{ $project->funds_end_date }}</td></tr>
-					<tr><td><a href="{{ $project->id }}">See Project</a></td></tr>
-				</table>
-			</td>
-		</tr>
-<<<<<<< HEAD
-		@foreach ($genres as $genre)
-		<tr><td>
-			<a href="?genre={{ $genre->id }}">
-				{{ $genre->genre }}
-			</a>
-		</td></tr>
-		@endforeach
-	</table>
-</div>
-<div class="col-md-8">
-	<table class="table table-bordered" id="projects-table">
-		@foreach ($projects as $project)
-			<tr>
-				<td>
-					<iframe width="100" height="56" src="{{ $project->video_url }}" frameborder="0" allowfullscreen></iframe>
-				</td>
-				<td>
-					<table>
-						<tr><td><b>Title: </b>{{ $project->project_title }}</td></tr>
-						<tr><td><b>Genre: </b>
-							{{{ $project->genre_list }}}
-						</td></tr>
-						<tr><td><b>Goal: </b>{{ $project->funds_goal }}</td></tr>
-						<tr><td><b>Currently Funded: </b>{{ $project->funds_current }}</td></tr>
-						<tr><td><b>Funding Ends: </b>{{ $project->funds_end_date }}</td></tr>
-						<tr><td><a href="{{ $project->id }}">See Project</a></td></tr>
-					</table>
-				</td>
-			</tr>
-		@endforeach
-	</table>
-	@if (!Input::has('genre'))
-		{{ $projects->links() }}
-	@endif
-</div>
-@stop
-=======
-	@endforeach
-</table>
--->
-<<<<<<< HEAD
-=======
->>>>>>> master
->>>>>>> 2ac514d03249d7447ce8d1c0132de69acc6a319a
